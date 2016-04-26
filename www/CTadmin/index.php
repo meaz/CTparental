f3e7cdf1f9728eac1f138ca406313642fbc194ae
Guillaume Marsat <guillaume.marsat@laposte.net>
2016-04-26 12:03:40 +0200

    renommé :         www/locale/ctadmin.pot -> locale/ctparental.pot 	nouveau fichier : locale/fr_FR/LC_MESSAGES/ctparental.mo 	renommé :         www/locale/fr_FR/LC_MESSAGES/fr.po -> locale/fr_FR/LC_MESSAGES/ctparental.po 	supprimé :        locale/fr_FR/LC_MESSAGES/fr.mo 	supprimé :        locale/fr_FR/LC_MESSAGES/fr.po 	modifié :         www/CTadmin/bl_categories_help.php 	modifié :         www/CTadmin/index.php 	nouveau fichier : www/CTadmin/locale.php 	modifié :         www/CTparental/bl_categories_help.php 	modifié :         www/CTparental/index.php 	nouveau fichier : www/CTparental/locale.php 	supprimé :        www/locale/fr_FR/LC_MESSAGES/fr.mo

--- www/CTadmin/index.php
+++ www/CTadmin/index.php
@@ -7,25 +7,7 @@
 </HEAD>
 <body>
 <?php
-// on détecte la langue system
-$LANG=getenv('LANG'); 
-if(isset($LANG)) {
-$tab=explode(".",getenv('LANG'));
-$domain=substr($tab[0],0,2);
-
-// set the locale into the instance of gettext 
-setlocale(LC_ALL,$LANG); // change by language, directory name fr_FR, not fr_FR.UTF-8 
-
-// Spécifie la localisation des tables de traduction
-// ce qui donne pour une variable $LANG='fr_FR.UTF-8' une répertoir ci dessous.
-// ./locale/fr_FR/LC_MESSAGES/
-bindtextdomain($domain, "./locale");
-
-// Choisit le domaine
-// ce qui nous donne un nom de fichier pour $LANG='fr_FR.UTF-8' de fr.mo
-textdomain($domain);
-// La traduction est cherché dans ./locale/fr_FR/LC_MESSAGES/fr.mo
-}
+include 'locale.php';
 
 function form_filter ($form_content)
 {
