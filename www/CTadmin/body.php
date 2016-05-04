09c4b1ef0580fbb96a6aed94dc3b5647779a77d5
Guillaume marsat <CTparental@laposte.net>
2016-05-04 21:25:29 +0200

    modifié :         www/CTadmin/bl_categories_help.php 	modifié :         www/CTadmin/body.php 	nouveau fichier : www/CTadmin/locale.php

--- www/CTadmin/body.php
+++ www/CTadmin/body.php
@@ -1,28 +1,5 @@
 <?php
-// on détecte la langue du système
-$LANG = getenv('LANG');
-//$LANG = "fr_FR.UTF-8";
-
-if (isset($LANG))
-{
-    $tab    = explode(".", getenv('LANG'));
-    //$domain = substr($tab[0], 0, 2);
-    $domain="ctparental";
-
-    // set the locale into the instance of gettext 
-    setlocale(LC_ALL, $LANG); // change by language, directory name fr_FR, not fr_FR.UTF-8 
-
-    // Spécifie la localisation des tables de traduction
-    // ce qui donne pour une variable « $LANG = 'fr_FR.UTF-8' » une répertoir ci dessous.
-    // ./locale/fr_FR/LC_MESSAGES/
-    bindtextdomain($domain, "/usr/share/locale");
-
-    // Choisit le domaine
-    // ce qui nous donne un nom de fichier pour « $LANG = 'fr_FR.UTF-8' » de fr.mo
-    textdomain($domain);
-    // La traduction se trouve dans ./locale/fr_FR/LC_MESSAGES/fr.mo
-}
-
+include 'locale.php';
 function form_filter ($form_content)
 {
     // réencodage iso + format unix + rc fin de ligne (ouf...)
