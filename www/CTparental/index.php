e82cc112a975cb3c690768975f914c79aa99c73c
Guillaume marsat <CTparental@laposte.net>
2016-12-22 10:41:53 +0100

    modifié :         debian/changelog 	modifié :         www/CTparental/index.php

--- www/CTparental/index.php
+++ www/CTparental/index.php
@@ -29,23 +29,6 @@ include 'locale.php';
                 <h1><?php echo gettext("Access to the page:");?></h1>
                 <h2><?php echo $_SERVER["HTTP_HOST"]; ?></h2>
                 <hr />
-                <h3><?php echo gettext("... has been denied for the following reason:");?></h3>
-                <h3>
-                    <strong>
-                    <?php
-                        exec ("/usr/bin/CTparental-bl-infos ".$_SERVER["HTTP_HOST"], $blinfosliste);
-                        
-                        //$var1 = $blinfosliste[0];
-                        //$tab1 = explode(" ", $var1);
-                        
-                        //foreach ($tab1 as $categorie)
-                        foreach ( $blinfosliste as $categorie )
-                        {
-                            echo "<a href='http://127.0.0.1/CTparental/bl_categories_help.php?cat=$categorie' title='categories help page'>$categorie</a><br />";
-                        }
-                    ?>
-                    </strong>
-                </h3>
             </div>
             
             <p class="text-justify text-warning">
