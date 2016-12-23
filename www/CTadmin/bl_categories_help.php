0bed90e739dce14f99926fd9802a44d8641f92a5
Guillaume marsat <CTparental@laposte.net>
2016-12-23 10:13:38 +0100

    modifié :         www/CTadmin/bl_categories_help.php

--- www/CTadmin/bl_categories_help.php
+++ www/CTadmin/bl_categories_help.php
@@ -1,25 +1,5 @@
 <?php
-# on détecte la langue system
-$LANG = getenv('LANG');
-
-if(isset($LANG))
-{
-    $tab    = explode(".", getenv('LANG'));
-    $domain = substr($tab[0], 0, 2);
-
-    // set the locale into the instance of gettext 
-    setlocale(LC_ALL, $LANG); // change by language, directory name fr_FR, not fr_FR.UTF-8 
-
-    // Spécifie la localisation des tables de traduction
-    // ce qui donne pour une variable $LANG='fr_FR.UTF-8' une répertoir ci dessous.
-    // ./locale/fr_FR/LC_MESSAGES/
-    bindtextdomain($domain, "./locale");
-
-    // Choisit le domaine
-    // ce qui nous donne un nom de fichier pour $LANG='fr_FR.UTF-8' de fr.mo
-    textdomain($domain);
-    // La traduction est cherché dans ./locale/fr_FR/LC_MESSAGES/fr.mo
-}
+include 'locale.php';
 $dirconf="/etc/CTparental";
 $bl_dir=$dirconf."/dnsfilter-available/";
 $l_title                      = gettext("Blacklist categories");
